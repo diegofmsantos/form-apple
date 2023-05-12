8bcf991faa41e35c58a0482d01086ba9f465c069
Diego Santos <diegofmsantos0@gmail.com>
2023-05-12 12:02:16 -0300

    fazendo ajustes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,20 @@
 import appleId from "../assets/apple-id.png";
 import { MdLogin } from 'react-icons/md';
 import { RiLockPasswordLine } from 'react-icons/ri';
-import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const schema = yup.object({
-    login: yup.string().required("Digite seu login.").min(3, "Você precisa inserir pelo menos 3 caracteres."),
-    password: yup.string().required("Digite sua senha.").min(6, "Sua senha precisa ter pelo menos 6 dígitos.")
-}).required()
-type FormData = yup.InferType<typeof schema>;
+type Inputs = {
+    login: string,
+    password: string
+}
 
 const Form = () => {
 
-    const { 
-        register, 
-        handleSubmit: onSubmit, 
-        watch, 
-        formState: { errors } } = useForm<FormData>({resolver: yupResolver(schema)});
-
-    const handleSubmit = (data: any) => {
-        console.log(data)
-    }
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
     
     return (
-        <form onSubmit={onSubmit(handleSubmit)} className='bg-black opacity-90 w-96 h-3/4 rounded-lg flex flex-col justify-start items-center border boder-white py-2 mb-6'>
+        <form onSubmit={handleSubmit(onSubmit)} className='bg-black opacity-90 w-96 h-3/4 rounded-lg flex flex-col justify-start items-center border boder-white py-2 mb-6'>
             <img className='w-40 pt-2 mb-6' src={appleId} alt="Logo Apple" />
             <div className='h-6 flex justify-center items-center gap-3'>
                 <MdLogin className='w-5 h-5 text-white' />
@@ -33,10 +23,11 @@ const Form = () => {
                 id="login" 
                 placeholder='Login' 
                 autoComplete='off' 
-                {...register("login")}
+                {...register("login", {required: true, minLength: 3})}
                 />
             </div>
-            <span className="h-8 text-red-500 text-sm">{errors?.login?.message}</span>
+            {errors?.login?.type === 'required' && <span className="h-8 text-red-500 text-sm">Digite seu login.</span>}
+            {errors?.login?.type === 'minLength' && <span className="h-8 text-red-500 text-sm">Seu login precisa ter mais de 3 caracteres.</span>}
             <div className='h-6 flex justify-center items-center gap-3 mt-4'>
                 <RiLockPasswordLine className='w-5 h-5 text-white' />
                 <input className='w-60 bg-transparent border-b-2 text-center outline-none text-white placeholder:text-sm mb-1' 
@@ -44,11 +35,12 @@ const Form = () => {
                 id="password" 
                 placeholder='Password' 
                 autoComplete='off' 
-                {...register("password")}
+                {...register("password", {required: true, minLength: 6})}
                 />
             </div>
-            <span className="h-8 text-red-500 text-sm mb-4">{errors.password?.message}</span>
-            <div className='text-white text-sm space-x-12 mb-8'>
+            {errors.password?.type === 'required' && <span className="h-8 text-red-500 text-sm">Digite sua senha.</span>}
+            {errors.password?.type === 'minLength' && <span className="h-8 text-red-500 text-sm">Sua senha precisa ter mais de 6 caracteres.</span>}
+            <div className='text-white text-sm space-x-12 mb-8 mt-8'>
                 <input type="checkbox" id="remember" /> Remember me
                 <a className='underline' href="#">Forgot Password?</a>
             </div>
