0b23435ad5d685a636ea524d2468681efecf65ca
Diego Santos <diegofmsantos0@gmail.com>
2023-05-16 22:01:44 -0300

    responsividade

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,12 +14,12 @@ const Form = () => {
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
     
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className='bg-black bg-opacity-80 w-96 h-3/4 rounded-lg flex flex-col justify-start items-center border boder-white py-2 mb-6'>
+        <form onSubmit={handleSubmit(onSubmit)} className='bg-black bg-opacity-80 w-96 h-3/4 rounded-lg flex flex-col justify-start items-center border boder-white py-2 mb-6 max-[450px]:w-[350px] max-[380px]:w-[320px]'>
             <img className='w-40 pt-2 mb-6' src={appleId} alt="Logo Apple" />
             <div className="h-12 text-center">
                 <div className='h-6 flex justify-center items-center gap-3'>
-                    <MdLogin className='w-5 h-5 text-white' />
-                    <input className='w-60 bg-transparent border-b-2 text-center outline-none text-white placeholder:text-sm mb-1'
+                    <MdLogin className='w-5 h-5 text-white max-[400px]:w-4 max-[400px]:h-4' />
+                    <input className='w-60 bg-transparent border-b-2 text-center outline-none text-white placeholder:text-sm mb-1 max-[400px]:w-52'
                     type="text"
                     id="login"
                     placeholder='Login'
@@ -32,8 +32,8 @@ const Form = () => {
             </div>
             <div className="h-12 text-center">
                 <div className='h-6 flex justify-center items-center gap-3 mt-4'>
-                    <RiLockPasswordLine className='w-5 h-5 text-white' />
-                    <input className='w-60 bg-transparent border-b-2 text-center outline-none text-white placeholder:text-sm mb-1'
+                    <RiLockPasswordLine className='w-5 h-5 text-white max-[400px]:w-4 max-[400px]:h-4' />
+                    <input className='w-60 bg-transparent border-b-2 text-center outline-none text-white placeholder:text-sm mb-1 max-[400px]:w-52'
                     type="password"
                     id="password"
                     placeholder='Password'
@@ -44,7 +44,7 @@ const Form = () => {
                 {errors.password?.type === 'required' && <span className="h-8 text-red-500 text-sm">Digite sua senha.</span>}
                 {errors.password?.type === 'minLength' && <span className="h-8 text-red-500 text-sm">Sua senha precisa ter mais de 6 caracteres.</span>}
             </div>
-            <div className='text-white text-sm space-x-12 mb-8 mt-8'>
+            <div className='text-white text-sm space-x-12 mb-8 mt-8 max-[400px]:space-x-9'>
                 <input type="checkbox" id="remember" /> Remember me
                 <a className='underline' href="#">Forgot Password?</a>
             </div>
